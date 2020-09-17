bffda232a3ba80878ce5a9746131465fca130eee
Arfiandimas <arfiandimas1929@gmail.com>
2020-09-17 23:13:07 +0700

    ADDED: chat app

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,11 +1,38 @@
-const sgMail = require('@sendgrid/mail')
-const sendgridAPIKey = process.env.SENDGRID_API_KEY
+"use strict";
+const nodemailer = require("nodemailer");
 
-sgMail.setApiKey(sendgridAPIKey)
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
 
-sgMail.send({
-    to: 'arfiandimas1929@gmail.com',
-    from: 'arfian.1018@students.amikom.ac.id',
-    subject: 'Nama saya Arfian Dimas Andi Permana!',
-    text: 'Saya berharap nama kamu sama denganku.'
-})
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <arfiandimas1929@gmail.com>', // sender address
+    to: "arfiandimas1929@gmail.com, arfian.1018@students.amikom.ac.id", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
